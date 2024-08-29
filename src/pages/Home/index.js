04e71fbaa3161471c925b7607fa5a628d1c1c45f
GabrielCaspirro <gabriel.gcd0u8@gmail.com>
2024-08-29 09:58:22 -0300

    AppRoutes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import Container from "../../components/Container";
+import Category, {categories,filterCategory} from "../../components/Category";
+import Card from "../../components/Card";
+import Footer from "../../components/Footer";
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <Banner image = "favoritos" />
+    <Container>
+       {categories.map((category,index) =>
+         <Category category={category}>
+          {filterCategory(index).map((video) => <Card id={video.id} key={video.id} />)}
+         </Category>
+         )}
+    </Container>
+    <Footer/>
+    </>
+);
+}
+
+export default Home;
