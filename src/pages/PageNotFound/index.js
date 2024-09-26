837fccafc3d03bb50cbb996248939d676fca2183
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-09-26 11:47:35 -0300

    Update index.js

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,5 +1,5 @@
 import erro404 from './erro404.png';
-import styles from "./PageNotFound.module.css";
+import styles from "./pageNotFound.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
